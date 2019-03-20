b569bea012dfe3ae7915ecf7833f01f2a8eb6849
Joshua <gamerguyjochi@gmail.com>
2019-03-20 10:44:56 -0600

    Just Capstone

--- AngularStuff/Hello/script.js
+++
@@ -1,5 +0,0 @@
-angular.module("helloApp", [])
-    .controller("HelloController", function($scope) {
-        $scope.greet = {};
-        $scope.greet.name = "Bob";
-    })
--- Web/simpleForm/script.js
+++
@@ -1,4 +0,0 @@
-function sendData(form) {
-    var formData = form.inputbox.value; 
-    document.getElementById( "response" ).innerHTML = formData;
-}
